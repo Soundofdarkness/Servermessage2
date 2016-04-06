0e15d7fa420c5f648dd6d514f08a354cf25b9c2a
Soundofdarkness <benni.bsf27@gmail.com>
2016-04-06 19:50:32 +0200

    First commit

---
+++ src/main/java/servermessage.java
@@ -0,0 +1,34 @@
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Soundofdarkness on 06.04.2016.
+ */
+
+    public class servermessage extends JavaPlugin {
+
+        public void onEnable(){
+            this.getLogger().info("Servermessage has been enabled. You are using verion 1.0");
+        }
+
+        public void onDisable(){
+            this.getLogger().info("Servermessage has been disabled.");
+        }
+
+        public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+            if(cmd.getName().equalsIgnoreCase("welcome")){ // Wenn /welcome ausgeführt wird wird ein Willkommen in den chat geschrieben
+                sender.sendMessage(ChatColor.GOLD + "Willkommen auf dem MMO-Server der Lux Caeli!");
+                this.getLogger().info( "Welcomed new players on the server");
+                return true;
+            } // Wenn das passiert, wird die Funktion abbrechen und true als Wert zurückgeben.
+            this.getLogger().info("Fehler beim ausführen von Welcome");
+            return false;
+
+        }
+
+
+
+    }
+
